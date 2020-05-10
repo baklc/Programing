4cc2364543346460a21773663fde3e2c2d95da13
baklc <bloodmoon0915@gmail.com>
2020-05-10 18:24:23 +0900

    version 1.2
    version 1.2
    playlist(재생목록) 보여주기
    재생목록 보여주고 다운로드 여부 묻는 기능 추가

--- pytube/pytube/!!youtube.py
+++ pytube/pytube/!!youtube.py
@@ -4,12 +4,12 @@ import urllib.request as req
 from pytube import YouTube
 import glob
 import os.path
+import sys
 
 from os import rename
 
 
 urlinput = input("url을 쓰시오")
-downloadtype = input("원하는 형식을 쓰시오 || mp4 / mp3")
 
 url = urlinput
 res = req.urlopen(url)
@@ -36,8 +36,19 @@ for i in kkkk:
         youtubelist_name.append(replaceAlltwo)
 
 
+for idd in youtubelist_name:
+    indddeex = youtubelist_name.index(idd)
+    print("\n|| {}번째 영상 : ".format(indddeex),youtubelist_name[indddeex])
+
+exitdd = input("\n 다운로드 하시겠습니까? Yes || No")
+if exitdd == 'No' or 'no' or 'n':
+    sys.exit()
 lllll = 0
 llllll = 0
+
+
+downloadtype = input("원하는 형식을 쓰시오 || mp4 / mp3")
+
 if downloadtype == "mp3":
     for listsss in youtubelist_name:
         numberlist = youtubelist_name.index(listsss)
@@ -54,10 +65,9 @@ else :
     for listssss in youtubelist_name:
         numberlistone = youtubelist_name.index(listssss)
 
-        print(youtubelist_name[numberlistone], "\n||" ,youtubelist[numberlistone])
+        print("\n||",youtubelist_name[numberlistone], "\n||" ,youtubelist[numberlistone])
 
         nnnnn = youtubelist[numberlistone]
-        print(nnnnn)
         YouTube(nnnnn).streams.first().download()
         if len(youtubelist_name) == llllll:
             break
