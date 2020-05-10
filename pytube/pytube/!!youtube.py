05709f29a35dc25af57be320ab7f376c4c758588
baklc <bloodmoon0915@gmail.com>
2020-05-10 18:13:22 +0900

    version 1.1
    python version 1.1
    mp4 기능 추가

--- pytube/pytube/!!youtube.py
+++ pytube/pytube/!!youtube.py
@@ -8,8 +8,10 @@ import os.path
 from os import rename
 
 
-#urlinput = input("url을 쓰시오")
-url = 'https://www.youtube.com/playlist?list=PLQt2tqIudYi3kjs6ww-f1gXRUhn6qpFFe'
+urlinput = input("url을 쓰시오")
+downloadtype = input("원하는 형식을 쓰시오 || mp4 / mp3")
+
+url = urlinput
 res = req.urlopen(url)
 
 soup = BeautifulSoup(res, 'html.parser')
@@ -35,26 +37,43 @@ for i in kkkk:
 
 
 lllll = 0
-for listsss in youtubelist_name:
-    numberlist = youtubelist_name.index(listsss)
+llllll = 0
+if downloadtype == "mp3":
+    for listsss in youtubelist_name:
+        numberlist = youtubelist_name.index(listsss)
 
-    print(youtubelist_name[numberlist], "\n||"  ,youtubelist[numberlist])
+        print(youtubelist_name[numberlist], "\n||"  ,youtubelist[numberlist])
 
-    nnnn = youtubelist[numberlist]
-    yt = YouTube(nnnn)
-    yt.streams.filter(only_audio=True).first().download()
-    lllll += 1
-    if len(youtubelist_name) == lllll:
-        break
-countingnum = 0;
+        nnnn = youtubelist[numberlist]
+        yt = YouTube(nnnn)
+        yt.streams.filter(only_audio=True).first().download()
+        lllll += 1
+        if len(youtubelist_name) == lllll:
+            break
+else :
+    for listssss in youtubelist_name:
+        numberlistone = youtubelist_name.index(listssss)
+
+        print(youtubelist_name[numberlistone], "\n||" ,youtubelist[numberlistone])
+
+        nnnnn = youtubelist[numberlistone]
+        print(nnnnn)
+        YouTube(nnnnn).streams.first().download()
+        if len(youtubelist_name) == llllll:
+            break
+
+countingnum = 0
+
 files = glob.glob("*.mp4")
-for x in files:
-    if not os.path.isdir(x):
-        filename = os.path.splitext(x)
-        try:
-            os.rename(x,filename[0] + '.mp3')
-        except:
-            pass
 
-    print("{} converting successful".format(countingnum))
-    countingnum += 1
+if downloadtype == "mp3":
+    for x in files:
+        if not os.path.isdir(x):
+            filename = os.path.splitext(x)
+            try:
+                os.rename(x,filename[0] + '.mp3')
+            except:
+                pass
+
+        print("{} converting successful".format(countingnum))
+        countingnum += 1
